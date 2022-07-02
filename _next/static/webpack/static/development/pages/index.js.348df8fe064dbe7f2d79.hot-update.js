dfd70e61e2ac09b5c99d1c720ae46d2ebe814061
Ryo Suzuki <ryosuzk@gmail.com>
2022-07-02 16:35:51 -0600

    Update

---
+++ _next/static/webpack/static/development/pages/index.js.348df8fe064dbe7f2d79.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./content/output/projects/selective-self-assembly.json":
+/*!**************************************************************!*\
+  !*** ./content/output/projects/selective-self-assembly.json ***!
+  \**************************************************************/
+/*! exports provided: id, name, description, title, authors, year, booktitle, publisher, conference, bodyContent, bodyHtml, dir, base, ext, sourceBase, sourceExt, default */
+/***/ (function(module) {
+
+module.exports = {"id":"selective-self-assembly","name":"Selective Self-Assembly","description":"Selective Self-Assembly using Re-Programmable Magnetic Pixels","title":"Selective Self-Assembly using Re-Programmable Magnetic Pixels","authors":["Martin Nisser","Yashaswini Makaram","Faraz Faruqi","Ryo Suzuki","Stefanie Mueller"],"year":2022,"booktitle":"In Proceedings of 2022 IEEE/RSJ In- ternational Conference on Intelligent Robots and Systems (IROS '22)","publisher":"ACM, New York, NY, USA","conference":{"name":"IROS 2022","fullname":"In Proceedings of 2022 IEEE/RSJ In- ternational Conference on Intelligent Robots and Systems (IROS 2022)","url":"https://iros2022.org/"},"bodyContent":"","bodyHtml":"","dir":"content/output/projects","base":"selective-self-assembly.json","ext":".json","sourceBase":"selective-self-assembly.md","sourceExt":".md"};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.348df8fe064dbe7f2d79.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.d108b4c230da0813ed22.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./content/output/projects/ar-and-robotics.json":
+/*!******************************************************!*\
+  !*** ./content/output/projects/ar-and-robotics.json ***!
+  \******************************************************/
+/*! exports provided: id, name, description, title, authors, year, conference, external, image, bodyContent, bodyHtml, dir, base, ext, sourceBase, sourceExt, default */
+/***/ (function(module) {
+
+module.exports = {"id":"ar-and-robotics","name":"Augmented Reality and Robotics","description":"A Survey and Taxonomy for AR-enhanced Human-Robot Interaction and Robotic Interfaces","title":"Augmented Reality and Robotics: A Survey and Taxonomy for AR-enhanced Human-Robot Interaction and Robotic Interfaces","authors":["Ryo Suzuki","Adnan Karim","Tian Xia","Hooman Hedayati","Nicolai Marquardt"],"year":2022,"conference":{"name":"CHI 2022","fullname":"The ACM CHI Conference on Human Factors in Computing Systems (CHI 2022)","url":"https://chi2022.acm.org/"},"external":"https://ilab.ucalgary.ca/ar-and-robotics/","image":"ar-and-robotics.jpg","bodyContent":"","bodyHtml":"","dir":"content/output/projects","base":"ar-and-robotics.json","ext":".json","sourceBase":"ar-and-robotics.md","sourceExt":".md"};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.d108b4c230da0813ed22.hot-update.js.map
